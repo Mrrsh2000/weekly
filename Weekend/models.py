d39d60a2dc0f8b9f655809e680832f8e0c2109f0
Rezvani <mrrsh80@gmail.com>
2021-12-04 12:35:57 +0330

    [fix] bug fix show media

--- Weekend/models.py
+++ Weekend/models.py
@@ -6,7 +6,7 @@ class UserFile(models.Model):
         verbose_name = 'کاربر'
         verbose_name_plural = 'کاربران'
 
-    html_file = models.FileField(upload_to='plans/', verbose_name="فایل html")
+    html_file = models.FileField(upload_to='media/', verbose_name="فایل html")
     code = models.IntegerField(verbose_name="کد کاربر")
     title = models.CharField(max_length=50, verbose_name="نام کاربر")
     created_at = models.DateTimeField(auto_now_add=True, blank=True, verbose_name="تاریخ ایجاد")
--- weekly/urls.py
+++ weekly/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -6,8 +7,10 @@ from Weekend.urls import router
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+from weekly import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/vi/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+                  path('admin/', admin.site.urls),
+                  path('api/vi/', include(router.urls)),
+                  path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
